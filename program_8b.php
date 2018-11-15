a9968fe1f3bf4e0ff87aacafaaa206566e933db6
manoj179 <42165292+manoj179@users.noreply.github.com>
2018-11-15 15:48:09 +0530

    Add files via upload

---
+++ program_8b.php
@@ -0,0 +1,16 @@
+<?php
+	$arr=array(
+		array(1,2,3),
+		array(4,5,6),
+		array(7,8,9));
+
+	echo "\n The Given Array is: "
+	for($i=0; $i<3; $i++)
+	{
+		for($j=0; $j<3; $j++)
+		{
+			echo $arr[i][j];
+		}
+	}
+
+?>
